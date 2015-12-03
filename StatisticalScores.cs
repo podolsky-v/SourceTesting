e5a5db94ac75a8c4450a0ab3ebed489b4454e760
Wladislaw Podolski <vpodolsky11@gmail.com>
2015-12-03 18:24:28 +0300

    directional runs for binary added

--- StatisticalScores.cs
+++ StatisticalScores.cs
@@ -48,16 +48,21 @@ namespace EntropySourceTesting
         /// </summary>
         /// <param name="sample">sample to analyse</param>
         /// <returns>longest run and total number of runs</returns>
-        public static int[] runsScores(int[] sample) {
+        public static int[] runsScores(int[] sample)
+        {
             int[] median = StandardFunctions.integerMedian(sample);
             //false: value is under the median
             //true: value is over the median
             //skipped: value is equal to the median
             List<bool> symbols = new List<bool>();
-            for(int i = 0; i < sample.Length; ++i) {
-                if(sample[i] < median[1]) {
+            for (int i = 0; i < sample.Length; ++i)
+            {
+                if (sample[i] < median[1])
+                {
                     symbols.Add(false);
-                } else if(sample[i] > median[0]) {
+                }
+                else if (sample[i] > median[0])
+                {
                     symbols.Add(true);
                 }
             }
@@ -67,20 +72,26 @@ namespace EntropySourceTesting
             int longestRunLength = 1;
             int runsNumber = 1;
             ///////////////////////////////////////////////////////////////////////////////////////
-            for(int i = 1; i < symbols.Count; ++i) {
-                if(symbols[i] != currentSymbol) {
+            for (int i = 1; i < symbols.Count; ++i)
+            {
+                if (symbols[i] != currentSymbol)
+                {
                     //the next run
-                    if(currentRunLength > longestRunLength) {
+                    if (currentRunLength > longestRunLength)
+                    {
                         longestRunLength = currentRunLength;
                     }
                     currentRunLength = 1;
-                    ++runsNumber;                    
-                } else {
+                    ++runsNumber;
+                }
+                else
+                {
                     //the same run
                     ++currentRunLength;
                 }
             }
-            if(currentRunLength > longestRunLength) {
+            if (currentRunLength > longestRunLength)
+            {
                 longestRunLength = currentRunLength;
             }
             return new int[] { longestRunLength, runsNumber };
@@ -91,19 +102,22 @@ namespace EntropySourceTesting
         /// </summary>
         /// <param name="sample">sample to analyse</param>
         /// <returns>maximal deviation from expected value</returns>
-        public static int[] excursionScores(int[] sample) {
+        public static int[] excursionScores(int[] sample)
+        {
             int initRemainder;
             int initAverage = StandardFunctions.integerAverage(sample, out initRemainder);
             decimal average = initAverage + initRemainder * 100 / sample.Length;
             decimal excursion = 0;
             decimal maxAbsValue = 0;
-            for(int i = 0; i < sample.Length; ++i) {
+            for (int i = 0; i < sample.Length; ++i)
+            {
                 excursion += sample[i] - average;
-                if(Math.Abs(excursion) > maxAbsValue) {
+                if (Math.Abs(excursion) > maxAbsValue)
+                {
                     maxAbsValue = Math.Abs(excursion);
                 }
             }
-            return new int[]{(int)maxAbsValue};
+            return new int[] { (int)maxAbsValue };
         }
 
         /// <summary>
@@ -111,28 +125,37 @@ namespace EntropySourceTesting
         /// </summary>
         /// <param name="sample">sample to analyse</param>
         /// <returns>longest run, total number of runs, total number of 1 or -1 (which greater)</returns>
-        public static int[] directionalRunsScores(int[] sample) {
+        public static int[] directionalRunsScores(int[] sample)
+        {
             //creation of derivatives array
             sbyte[] derivatives = new sbyte[sample.Length - 1];
             int numberOfOnes = 0;
             int numberOfMinusOnes = 0;
-            for(int i = 0; i < derivatives.Length - 1; ++i) {
-                if(sample[i] < sample[i + 1]) {
+            for (int i = 0; i < derivatives.Length - 1; ++i)
+            {
+                if (sample[i] < sample[i + 1])
+                {
                     derivatives[i] = 1;
                     ++numberOfOnes;
-                } else if(sample[i] > sample[i + 1]) {
+                }
+                else if (sample[i] > sample[i + 1])
+                {
                     derivatives[i] = -1;
                     ++numberOfMinusOnes;
-                } else {
+                }
+                else
+                {
                     derivatives[i] = 0;
                 }
             }
             //skipping leading 0
             int currentPosition = 0;
-            while(derivatives[currentPosition] == 0 && currentPosition < derivatives.Length - 1) {
+            while (derivatives[currentPosition] == 0 && currentPosition < derivatives.Length - 1)
+            {
                 ++currentPosition;
             }
-            if(derivatives[currentPosition] == 0) {
+            if (derivatives[currentPosition] == 0)
+            {
                 return new int[] { 0, 0, 0 };
             }
             ///////////////////////////////////////////////////////////////////////////////////////
@@ -141,36 +164,44 @@ namespace EntropySourceTesting
             int runsNumber = 1;
             int currentRunLength = 1;
             int longestRunLength = 1;
-            for(; currentPosition < derivatives.Length; ++currentPosition) {
-                if(derivatives[currentPosition] == -currentValue) {
+            for (; currentPosition < derivatives.Length; ++currentPosition)
+            {
+                if (derivatives[currentPosition] == -currentValue)
+                {
                     //the next run (zeros do not break the run)
                     ++runsNumber;
-                    if(currentRunLength > longestRunLength) {
+                    if (currentRunLength > longestRunLength)
+                    {
                         longestRunLength = currentRunLength;
                     }
                     currentRunLength = 1;
-                } else {
+                }
+                else
+                {
                     //the same run
                     ++currentRunLength;
                 }
             }
-            if(currentRunLength > longestRunLength) {
+            if (currentRunLength > longestRunLength)
+            {
                 longestRunLength = currentRunLength;
             }
-            return new int[] { 
-                longestRunLength, runsNumber, Math.Max(numberOfOnes, numberOfMinusOnes) 
+            return new int[] {
+                longestRunLength, runsNumber, Math.Max(numberOfOnes, numberOfMinusOnes)
             };
-        }        
+        }
 
         /// <summary>
         /// calculates covariance score
         /// </summary>
         /// <param name="sample">sample to analyse</param>
         /// <returns>covariance estimate</returns>
-        public static int[] covarianceScores(int[] sample) {
+        public static int[] covarianceScores(int[] sample)
+        {
             int average = StandardFunctions.integerAverage(sample);
             int[] counts = new int[sample.Length - 1];
-            for(int i = 0; i < counts.Length; ++i) {
+            for (int i = 0; i < counts.Length; ++i)
+            {
                 counts[i] = (sample[i] - average) * (sample[i + 1] - average);
             }
             return new int[] { StandardFunctions.integerAverage(counts) };
@@ -181,9 +212,10 @@ namespace EntropySourceTesting
         /// </summary>
         /// <param name="sample">sample to analyse</param>
         /// <returns>number of values to collision: minimal, maximal and average</returns>
-        public static int[] collisionScores(int[] sample) {
+        public static int[] collisionScores(int[] sample)
+        {
             int[] collisions = StandardFunctions.allCollisions(sample);
-            return new int[]{collisions.Min(), collisions.Max(), 
+            return new int[]{collisions.Min(), collisions.Max(),
                     StandardFunctions.integerAverage(collisions.ToArray())
             };
         }
@@ -229,7 +261,7 @@ namespace EntropySourceTesting
         /// <param name="sample">sample to analyse</param>
         /// <returns>longest run and total number of runs</returns>
         public static int[] runsScores(BitArray sample)
-        {            
+        {
             //median=0.5
             //false: value is under the median
             //true: value is over the median
@@ -282,6 +314,28 @@ namespace EntropySourceTesting
                 }
             }
             return new int[] { (int)Decimal.Round(maxAbsValue) };
+        }
+
+        /// <summary>
+        /// calculates directional runs score
+        /// </summary>
+        /// <param name="sample">sample to analyse</param>
+        /// <returns>longest run, total number of runs, total number of 1 or -1 (which greater)</returns>
+        public static int[] directionalRunsScores(BitArray sample)
+        {
+            //byte length
+            int count = sample.Count / 8;
+            int[] hammingWeights = new int[count];
+            int sum;
+            for (int j = 0; j < count; ++j)
+            {
+                sum = 0;
+                for (int i = 0; i < 8; ++i)
+                    if(sample[8 * j + i])
+                        ++sum;
+                hammingWeights[j] = sum;
+            }
+            return directionalRunsScores(hammingWeights);
         }
     }
 }
